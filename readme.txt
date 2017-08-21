7b833c4b4223aa8ffb7c7a01da431c3b1d1e212f
mjhead <andremjhead@gmail.com>
2017-08-21 17:16:35 +0300

    UPD: change version

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: TemplateMonster 2002
 Tags: testimonials, reviews, custom post type, slider, swiper, cherry framework, elementor
 Requires at least: 4.5
 Tested up to: 4.8.1
-Stable tag: 1.2.0
+Stable tag: 1.1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- testimonials.php
+++ testimonials.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cherry Testimonials
  * Plugin URI:
  * Description: A testimonials management plugin for WordPress.
- * Version:     1.2.0
+ * Version:     1.1.0.1
  * Author:      JetImpex
  * Author URI:  https://jetimpex.com/wordpress/
  * Text Domain: cherry-testi
@@ -104,7 +104,7 @@ if ( ! class_exists( 'TM_Testimonials_Plugin' ) ) {
 			 *
 			 * @since 1.0.0
 			 */
-			define( 'TM_TESTI_VERSION', '1.2.0' );
+			define( 'TM_TESTI_VERSION', '1.1.0.1' );
 
 			/**
 			 * Set the name for the `meta_key` value in the `wp_postmeta` table.
